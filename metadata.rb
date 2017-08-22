1b1748128989c31e9f17a30d1686f7d99d2914be
John Snow <thelunaticscripter@outlook.com>
2017-08-22 11:24:51 -0400

    Set up initial env cookbook
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,5 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures prod_environments'
 long_description 'Installs/Configures prod_environments'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/prod_environments/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/prod_environments'
--- recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: prod_environments
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
