b5dc22217829436b370240c9ee312d4d07b5a6b9
John Snow <thelunaticscripter@outlook.com>
2017-08-24 13:01:15 -0400

    add example for attributes
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- environments/dev.rb
+++ environments/dev.rb
@@ -1,3 +1,16 @@
 name 'dev'
 description 'The Development Environment'
 cookbook 'workflow_testing', '= 0.1.3'
+default_attributes workflow_testing: {
+                     test: 'dev',
+                   },
+                   audit: {
+                     fetcher: 'chef-server-automate',
+                     reporter: 'chef-server-automate',
+                     profiles: [
+                       {
+                         name: 'CIS Microsoft Windows Server 2012 R2 Benchmark Level 1 - Memeber Server',
+                         compliance: 'admin/cis-windows2012r2-level1-memeberserver',
+                       },
+                     ],
+                   }
